65c2a153e1d49a793b2f89d0d442f54703a29fa4
sadeq76 <sadeqshahmoradi76@gmail.com>
2023-12-10 12:59:28 +0330

    remove defineProps, defineEmits imports

--- src/components/AInput.vue
+++ src/components/AInput.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, defineProps, defineEmits } from 'vue'
+import { ref, computed } from 'vue'
 
 import { type InputProps } from '@/models/props/input'
 
